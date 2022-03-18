aa41974f1559fb6ede00b5f29144bad9ff90ea35
Edward-Watts <edwardwadjei@gmail.com>
2022-03-18 10:13:50 +0000

    edit(): change burger ingredients image

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ import classes from './CheckoutSummary.module.css';
 const checkoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
-            <h1>We hope it tastes well!</h1>
+            <h1>Enjoy your delicious burger!</h1>
             <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
